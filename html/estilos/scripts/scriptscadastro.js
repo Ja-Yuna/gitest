00d5c322be3a029238261f78ddd2e26ea34ee89d
Ja-Yuna <chaozinhaa@gmail.com>
2022-04-05 22:39:34 -0300

    submit files

---
+++ html/estilos/scripts/scriptscadastro.js
@@ -0,0 +1,11 @@
+function onSubmit() {
+    var nome = document.getElementsByTagName("input")[0];
+    if (nome.value === "") {
+        alert("Informe nome");
+        nome.focus();
+        return;
+    }
+
+    document.getElementsByTagName("form")[0].submit();
+}
+
